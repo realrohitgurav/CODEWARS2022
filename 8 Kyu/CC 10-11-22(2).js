12293fbd4a3957574598b3dc72b20ba57bab2587
Rohit Gurav <thisisrohitgurav@gmail.com>
2022-11-10 23:09:25 +0530

    added CC 10-11-22(2)

---
+++ 8 Kyu/CC 10-11-22(2).js
@@ -0,0 +1,13 @@
+// DESCRIPTION:
+// Make a function that will return a greeting statement that uses an input; your program should return, 
+
+// "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+// Solution:-
+// 1)
+function greeting(input){
+    return `Hello, ${input} how are you doing today?`
+}
+greeting("Rohit")
