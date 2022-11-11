873043b0224679d85f494121ba959347686c7e88
Rohit Gurav <thisisrohitgurav@gmail.com>
2022-11-11 23:16:00 +0530

    added CC 11-11-22

---
+++ 8 Kyu/CC 11-11-22.js
@@ -0,0 +1,16 @@
+// DESCRIPTION:
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+// Solution:-
+// 1)
+
+function booleanToString(value){
+    return value.toString()
+}
+booleanToString(true)
+
+// 2)
+const boolToString = valueTo => {return valueTo.toString()}
+boolToString(false)
