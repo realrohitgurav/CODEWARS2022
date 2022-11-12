9c7787ce073c8da052e26b1e37c7f5d85a5a636d
Rohit Gurav <thisisrohitgurav@gmail.com>
2022-11-12 22:06:15 +0530

    added CC 12-11-22(1)

---
+++ 8 Kyu/CC 12-11-22(1).js
@@ -0,0 +1,12 @@
+// DESCRIPTION:
+// Code as fast as you can! You need to double the integer and return it.
+
+// Solution:-
+// 1)
+const doubledInteger = int => {return int*2}
+doubledInteger(2)
+
+// 2)
+function doubleInteger(i) {
+    return i*2;
+  }
