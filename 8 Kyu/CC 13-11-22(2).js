8885e6297466fd58ac3de9a2e1cedf57f535d046
Rohit Gurav <thisisrohitgurav@gmail.com>
2022-11-13 21:08:30 +0530

    added CC 13-11-22(2)

---
+++ 8 Kyu/CC 13-11-22(2).js
@@ -0,0 +1,12 @@
+// DESCRIPTION:
+// Write a function which converts the input string to uppercase.
+
+// Solution:-
+// 1)
+const stringToUppercase = str => {return str.toUpperCase()}
+stringToUppercase("hello worlddddddddd");
+
+// 2)
+function makeUpperCase(str) {
+    return str.toUpperCase()
+  }
