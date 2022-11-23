a4fb13dd9b67b9b5806cbd5b8e77cd235089d714
Rohit Gurav <thisisrohitgurav@gmail.com>
2022-11-23 21:40:02 +0530

    added CC 23-11-22(1)

---
+++ 8 Kyu/CC 23-11-22(1).js
@@ -0,0 +1,18 @@
+// DESCRIPTION:
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+
+// var websites = [];
+
+// Solution:-
+// 1)
+
+var websites = []
+websites.unshift("codewars")
+console.log(websites);
+
+// 2)
+websites[0]="codewars"
+
