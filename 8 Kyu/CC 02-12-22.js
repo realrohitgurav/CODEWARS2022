21272837133e236153b88543bb1807f6d399aa50
Rohit Gurav <thisisrohitgurav@gmail.com>
2022-12-02 23:21:15 +0530

    added CC 02-12-22

---
+++ 8 Kyu/CC 02-12-22.js
@@ -0,0 +1,22 @@
+// DESCRIPTION:
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+
+// Solution:-
+// 1)
+const doubleChar = (str) => str.split("").map(c => c + c).join("");
+
+// 2)
+function doubleChar(str) {
+	var word = '';
+    for (var i = 0; i < str.length; i++){
+  	    word = word + str[i] + str[i];
+};
+  return word;
+};
+
+
--- 8 Kyu/CC 10-11-22.js
+++ 8 Kyu/CC 10-11-22.js
@@ -1,5 +1,6 @@
 // DESCRIPTION:
-// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+// Consider an array/list of sheep where some sheep may be missing from their place. 
+// We need a function that counts the number of sheep present in the array (true means present).
 
 // For example,
 
@@ -35,4 +36,4 @@ function countSheeps(arrayOfSheep) {
         num++;
         
     return num;
-  }
+}
