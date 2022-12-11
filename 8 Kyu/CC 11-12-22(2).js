cf6e5c0cde2a0c13c09e8087921af1dc4e29a955
Rohit Gurav <thisisrohitgurav@gmail.com>
2022-12-11 22:00:44 +0530

    added CC 11-12-22(2)

---
+++ 8 Kyu/CC 11-12-22(2).js
@@ -0,0 +1,23 @@
+// Write a function to get the first element(s) of a sequence. Passing a parameter n (default=1) will return the first n element(s) of the sequence.
+
+// If n == 0 return an empty sequence []
+
+// Examples
+// var arr = ['a', 'b', 'c', 'd', 'e'];
+// first(arr) //=> ['a'];
+// first(arr, 2) //=> ['a', 'b']
+// first(arr, 3) //=> ['a', 'b', 'c'];
+// first(arr, 0) //=> [];
+
+// Solution:-
+// 1)
+
+function first(arr, n = 1) {
+  return arr.slice(0, n);
+}
+first(["a", "b", "c", "d", "e"], 3);
+
+// 2)
+function first(arr, n = 1) {
+  return n ? arr.slice(0, n) : [];
+}
