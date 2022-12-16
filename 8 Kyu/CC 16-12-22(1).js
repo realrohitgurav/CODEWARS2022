8159ca4fd56ea3a6dc1d9e7cd071b298604b1859
Rohit Gurav <thisisrohitgurav@gmail.com>
2022-12-16 12:53:05 +0530

    added CC 16-12-22(1)

---
+++ 8 Kyu/CC 16-12-22(1).js
@@ -0,0 +1,30 @@
+// DESCRIPTION:
+// Can you implement a function that will return number of words in a string?
+
+// You have to ensure that spaces in string is a whitespace for real.
+
+// Let's take a look on some examples:
+
+// countWords("Hello"); // returns 1 as int
+// countWords("Hello, World!") // returns 2
+// countWords("No results for search term `s`") // returns 6
+// countWords(" Hello") // returns 1
+// // ... and so on
+// What kind of tests we made for your code:
+
+// Function have to count words and not spaces. You have to be sure that you doing it right
+// Empty string has no words.
+// String with spaces around should be trimmed.
+// Non-whitespace (ex. breakspace, unicode chars) should be treated as a delimiter
+// Doublecheck that words with chars like -, ', ` are counted right.
+
+// Solution:-
+// 1)
+function countWords(str) {
+  let string = str.replace(",", "").split(" ");
+  return string.length;
+}
+countWords("Hello, World!");
+
+// 2)
+const countWords = (str) => str.split(/\s+/).filter((x) => x).length;
